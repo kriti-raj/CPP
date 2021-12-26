81bbbf4aa0b9913bfe138babf708001bab959ded
Kriti Raj <6517kritiraj@gmail.com>
2021-12-27 00:55:34 +0530

    tut2 modified

---
+++ tut2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 14, b = 15;
+    float pi = 3.14;
+    char c = 'u';
+    cout<<"This is tutorial number 2. \nHere the value of a is "<<a<<".\nThe value of b is "<<b;
+    cout<<".\nThe value of pi is "<<pi;
+    cout<<".\nThe value of c is "<<c<<".";
+    return 0;
+}
