c3ead10f9c51bd6a4baad7f95289f4bfa2ff3594
Kriti Raj <6517kritiraj@gmail.com>
2021-12-27 03:51:54 +0530

    tut3 added

--- tut3.cpp
+++ tut3.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
+
+int a = 45;
+
 int main()
 {
-    cout<<"Hello World";
+    int a;
+    cout<<"Enter a number:\t";
+    cin>>a;
+    cout<<"Local a is:\t"<<a<<endl;
+    cout<<"Global a is:\t"<<::a;
+
     return 0;
 }
