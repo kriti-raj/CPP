72d0bb8210a5f4bd915fa4bbe4481dcd3ecc0ff4
Kriti Raj <6517kritiraj@gmail.com>
2021-12-27 01:20:14 +0530

    modified tut2

--- tut2.cpp
+++ tut2.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
+int glob = 5; 
+void sum()
+{
+    cout<<glob;
+}
+
 int main()
 {
-    int a = 14, b = 15;
+    int glob = 9;
+    glob = 543;
+    int a, b;
+    cout<<"Enter the value of a:\t";
+    cin>>a;
+    cout<<"Enter the value of b:\t";
+    cin>>b;
     float pi = 3.14;
     char c = 'u';
+    sum();
+    bool boolean = true;
+    cout<<"\n"<<glob<<"\t"<<boolean<<"\n";
     cout<<"This is tutorial number 2. \nHere the value of a is "<<a<<".\nThe value of b is "<<b;
     cout<<".\nThe value of pi is "<<pi;
     cout<<".\nThe value of c is "<<c<<".";
